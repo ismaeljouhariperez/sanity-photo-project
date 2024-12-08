67e0b2897cb9f7b89f7a8a553dc72e6a9de6f258
Ismaël Perez <ismael.jouhari@gmail.com>
2024-12-08 22:39:04 +0100

    Defining schema types for Sanity

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,8 +1,10 @@
 import { SchemaTypeDefinition } from 'sanity'
 import photo from './photo'
 import collection from './collection'
+import { project } from './project'
 
 export const schemaTypes: SchemaTypeDefinition[] = [
   photo,
   collection,
+  project,
 ]
---
+++ sanity/schemas/project.ts
@@ -0,0 +1,22 @@
+export const project = {
+  name: 'project',
+  title: 'Projets',
+  type: 'document',
+  fields: [
+    {
+      name: 'title',
+      title: 'Titre',
+      type: 'string',
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'text',
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+    }
+  ]
+} 
