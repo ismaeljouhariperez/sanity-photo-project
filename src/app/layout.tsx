fa3f9a009cf19ef01e8f3fa35a1964b0b210dac5
Ismaël Perez <ismael.jouhari@gmail.com>
2025-02-12 22:32:59 +0100

    [UPDATE] Minor changes on previous commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import React from 'react'
 import { Inter } from 'next/font/google'
 import { AnimatePresence } from 'framer-motion'
 import { ThemeProvider } from 'next-themes'
-import { MenuProvider } from './context/MenuContext'
 import Header from './components/Header'
 import ThemeToggle from './components/ThemeToggle'
 import './globals.css'
@@ -20,7 +19,6 @@ export default function RootLayout({
     <html lang="fr" suppressHydrationWarning>
       <body className={`${inter.className} transition-colors duration-200 bg-white dark:bg-gray-900 text-black dark:text-white`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-          <MenuProvider>
             <Header />
             <ThemeToggle />
             <main>
@@ -28,7 +26,6 @@ export default function RootLayout({
                 {children}
               </AnimatePresence>
             </main>
-          </MenuProvider>
         </ThemeProvider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,16 @@ import React from 'react'
 // import Image from 'next/image'
 import Link from 'next/link'
 import { motion } from 'framer-motion'
-import { useMenu } from './context/MenuContext'
 import ImageKitImage from './components/ImageKitImage'
 
 export default function Home() {
-  const { setMenuText } = useMenu()
 
   return (
     <div className="h-[calc(100vh-5.5rem)] grid grid-cols-2 gap-4 p-4 items-center justify-items-center">
       <motion.div
         layoutId="bw-image"
         className="relative flex items-center w-80 aspect-[3/4]"
-        onMouseEnter={() => setMenuText('Noir et Blanc')}
-        onMouseLeave={() => setMenuText('Index')}>
+        >
         <Link href="/projects/noir-et-blanc">
           <ImageKitImage 
             src="default-image.jpg"
@@ -29,8 +26,7 @@ export default function Home() {
       <motion.div
         layoutId="color-image"
         className="relative flex items-center w-80 aspect-[3/4]"
-        onMouseEnter={() => setMenuText('Couleur')}
-        onMouseLeave={() => setMenuText('Index')}>
+       >
         <Link href="/projects/couleur">
         <ImageKitImage 
             src="default-image.jpg"
