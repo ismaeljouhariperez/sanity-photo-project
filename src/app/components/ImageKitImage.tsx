7d52d072c6308a1958bda8ec8b6331e6d1a74468
Ismael Perez <ismael.jouhari@gmail.com>
2025-02-15 19:01:00 +0100

    [UPDATE] Minor change on Image Kit component

--- src/app/components/ImageKitImage.tsx
+++ src/app/components/ImageKitImage.tsx
@@ -3,18 +3,20 @@ import Image from 'next/image'
 interface ImageKitImageProps {
   src: string
   alt: string
-  width: number
-  height: number
+  width?: number
+  height?: number
   className?: string
+  fill?: boolean
 }
 
-export default function ImageKitImage({ src, alt, width, height, className }: ImageKitImageProps) {
+export default function ImageKitImage({ src, alt, width, height, className, fill }: ImageKitImageProps) {
   return (
     <Image
       src={`https://ik.imagekit.io/ismaelahab/${src}`}
       alt={alt}
       width={width}
       height={height}
+      fill={fill}
       className={className}
     />
   )
